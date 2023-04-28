5780233dfd0ad89685809a355c840eecce5e8f97
KrishnaMurar <114321792+KrishnaMurar@users.noreply.github.com>
2023-04-28 08:50:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import NewExpense from './Components/ExpenseForm/NewExpense' 
+ import Expenses from './Components/Expenses/Expenses'
+
+const App = () => {
+  const expense =[
+    {
+      id : 'e1',
+      title : "Car Wash",
+      Date : new Date(2023, 5, 28),
+      Amount :  520,
+      location : "Delhi"
+    },
+    {
+      id : 'e2',
+      title : "Shopping",
+      Date : new Date(2023, 1, 28),
+      Amount :  400,
+      location : "Noida"
+    },
+    {
+      id : 'e3',
+      title : "Car Travelling",
+      Date : new Date(2023, 2, 18),
+      Amount :  520,
+      location : "Gurgoan"
+    }
+  ];
+
+  const addExpenseHandler = expenses =>{
+    console.log('In app.js');
+    console.log(expenses);
+  }
+  
+  return (
+    <div>
+      
+      <NewExpense onAddExpense ={addExpenseHandler} />
+      <Expenses items={expense} />
+    </div>
+   
+  );
+}
+ export default App;
